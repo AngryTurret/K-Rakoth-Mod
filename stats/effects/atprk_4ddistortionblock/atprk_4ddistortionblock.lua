e7d665fdcec4bc9c1e74e8c5bf860a8998b936d5
Angry Turret <100763026+AngryTurret@users.noreply.github.com>
2022-03-02 14:32:59 +0300

    Add files via upload

---
+++ stats/effects/atprk_4ddistortionblock/atprk_4ddistortionblock.lua
@@ -0,0 +1,13 @@
+function init()
+   effect.addStatModifierGroup({{stat = "atprk_4ddistortionResistance", amount = 0.25}, {stat = "atprk_4ddistortionStatusImmunity", amount = 1}})
+
+   script.setUpdateDelta(0)
+end
+
+function update(dt)
+
+end
+
+function uninit()
+  
+end
